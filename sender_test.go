85420256ae0c59d64c75dc57f59a5c538d1c1e77
Liam Stanley <me@liamstanley.io>
2016-11-22 13:47:15 -0500

    implement EventLimiter for rate limiting of sending events

--- sender_test.go
+++ sender_test.go
@@ -7,6 +7,7 @@ package girc
 import (
 	"bytes"
 	"testing"
+	"time"
 )
 
 func TestSender(t *testing.T) {
@@ -20,4 +21,36 @@ func TestSender(t *testing.T) {
 	if e.Raw()+"\r\n" != bw.String() {
 		t.Errorf("serverSender{writer: newEncoder(bytes.Buffer)} = %v, want %v", bw, e.String()+"\r\n")
 	}
+}
+
+func TestEventLimiter(t *testing.T) {
+	events := []*Event{}
+	sendFunc := func(event *Event) error {
+		events = append(events, event)
+
+		return nil
+	}
+
+	limiter := NewEventLimiter(1, 150*time.Millisecond, sendFunc)
+
+	var e1, e2 *Event
+	go func() {
+		if err := limiter.SendAll(e1, e2); err != nil {
+			t.Fatalf("SendAll gave: %v", err)
+		}
+	}()
+
+	// Checking it immediately should yield 1 time.
+	if len(events) > 1 {
+		t.Fatalf("limiter has %v events, wanted 0 or 1", len(events))
+	}
+
+	time.Sleep(500 * time.Millisecond)
+
+	// It should now show a length of two.
+	if len(events) != 2 {
+		t.Fatalf("limiter has %v events, wanted 2", len(events))
+	}
+
+	limiter.Stop()
 }
