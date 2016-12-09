e3f207c285e1c67389f8d43c37dfd8d8664b92d6
Liam Stanley <me@liamstanley.io>
2016-12-09 06:43:52 -0500

    main.go->client.go, add doc.go for main documentation

--- client.go
+++ client.go
@@ -2,17 +2,6 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-// Package girc provides a high level, yet flexible IRC library for use with
-// interacting with IRC servers. girc has support for user/channel tracking,
-// as well as a few other neat features (like auto-reconnect).
-//
-// Much of what girc can do, can also be disabled. The goal is to provide a
-// solid API that you don't necessarily have to work with out of the box if
-// you don't want to.
-//
-// See "examples/simple/main.go" for a brief and very useful example taking
-// advantage of girc, that should give you a general idea of how the API
-// works.
 package girc
 
 import (
---
+++ doc.go
@@ -0,0 +1,16 @@
+// Copyright 2016 Liam Stanley <me@liamstanley.io>. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+// Package girc provides a high level, yet flexible IRC library for use with
+// interacting with IRC servers. girc has support for user/channel tracking,
+// as well as a few other neat features (like auto-reconnect).
+//
+// Much of what girc can do, can also be disabled. The goal is to provide a
+// solid API that you don't necessarily have to work with out of the box if
+// you don't want to.
+//
+// See "examples/simple/main.go" for a brief and very useful example taking
+// advantage of girc, that should give you a general idea of how the API
+// works.
+package girc
