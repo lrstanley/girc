714a315ef050008a4507c891bd57724ffe2ff2ec
Liam Stanley <me@liamstanley.io>
2017-01-03 11:05:27 -0500

    yay new year

--- tags.go
+++ tags.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Liam Stanley <me@liamstanley.io>. All rights reserved.
+// Copyright 2016-2017 Liam Stanley <me@liamstanley.io>. All rights reserved.
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
