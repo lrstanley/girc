b102affd93afa2bfd08bb61a37b0855086c542df
Liam Stanley <me@liamstanley.io>
2018-09-05 20:22:44 -0400

    Event.Pretty(): fill in reason even if none is given

--- event.go
+++ event.go
@@ -387,7 +387,11 @@ func (e *Event) Pretty() (out string, ok bool) {
 		return fmt.Sprintf("[*] %s invited to %s by %s", e.Params[0], e.Trailing, e.Source.Name), true
 	}
 
-	if e.Command == KICK && len(e.Params) == 2 {
+	if e.Command == KICK && len(e.Params) >= 2 {
+		if e.Trailing == "" && len(e.Params) == 3 {
+			e.Trailing = e.Params[2]
+		}
+
 		return fmt.Sprintf("[%s] *** %s has kicked %s: %s", e.Params[0], e.Source.Name, e.Params[1], e.Trailing), true
 	}
 
