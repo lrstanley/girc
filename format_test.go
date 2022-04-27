c2a51e819cc23a4b634df9c60fb6a87a547e14d8
Liam Stanley <me@liamstanley.io>
2022-04-26 22:08:13 -0400

    feat: add fuzz testing to format-related methods
    BREAKING: this bumps the required version of Go to 1.18, as this is
    the first version that has fuzz testing support.
    
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- format_test.go
+++ format_test.go
@@ -7,6 +7,7 @@ package girc
 import (
 	"strings"
 	"testing"
+	"unicode/utf8"
 )
 
 func BenchmarkFormat(b *testing.B) {
@@ -47,192 +48,281 @@ func BenchmarkStripRawLong(b *testing.B) {
 	}
 }
 
+var testsFormat = []struct {
+	name string
+	test string
+	want string
+}{
+	{name: "middle", test: "test{red}test{c}test", want: "test\x0304test\x03test"},
+	{name: "middle with bold", test: "test{red}{b}test{c}test", want: "test\x0304\x02test\x03test"},
+	{name: "start, end", test: "{red}test{c}", want: "\x0304test\x03"},
+	{name: "start, middle, end", test: "{red}te{red}st{c}", want: "\x0304te\x0304st\x03"},
+	{name: "partial", test: "{redtest{c}", want: "{redtest\x03"},
+	{name: "inside", test: "{re{c}d}test{c}", want: "{re\x03d}test\x03"},
+	{name: "nothing", test: "this is a test.", want: "this is a test."},
+	{name: "fg and bg", test: "{red,yellow}test{c}", want: "\x0304,08test\x03"},
+	{name: "just bg", test: "{,yellow}test{c}", want: "test\x03"},
+	{name: "just red", test: "{red}test", want: "\x0304test"},
+	{name: "just cyan", test: "{cyan}test", want: "\x0311test"},
+}
+
+func FuzzFormat(f *testing.F) {
+	for _, tc := range testsFormat {
+		f.Add(tc.test)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		got := Fmt(orig)
+		got2 := Fmt(got)
+
+		if utf8.ValidString(orig) {
+			if !utf8.ValidString(got) {
+				t.Errorf("produced invalid UTF-8 string %q", got)
+			}
+
+			if !utf8.ValidString(got2) {
+				t.Errorf("produced invalid UTF-8 string %q", got2)
+			}
+		}
+	})
+}
+
 func TestFormat(t *testing.T) {
-	type args struct {
-		text string
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{name: "middle", args: args{text: "test{red}test{c}test"}, want: "test\x0304test\x03test"},
-		{name: "middle with bold", args: args{text: "test{red}{b}test{c}test"}, want: "test\x0304\x02test\x03test"},
-		{name: "start, end", args: args{text: "{red}test{c}"}, want: "\x0304test\x03"},
-		{name: "start, middle, end", args: args{text: "{red}te{red}st{c}"}, want: "\x0304te\x0304st\x03"},
-		{name: "partial", args: args{text: "{redtest{c}"}, want: "{redtest\x03"},
-		{name: "inside", args: args{text: "{re{c}d}test{c}"}, want: "{re\x03d}test\x03"},
-		{name: "nothing", args: args{text: "this is a test."}, want: "this is a test."},
-		{name: "fg and bg", args: args{text: "{red,yellow}test{c}"}, want: "\x0304,08test\x03"},
-		{name: "just bg", args: args{text: "{,yellow}test{c}"}, want: "test\x03"},
-		{name: "just red", args: args{text: "{red}test"}, want: "\x0304test"},
-		{name: "just cyan", args: args{text: "{cyan}test"}, want: "\x0311test"},
-	}
-
-	for _, tt := range tests {
-		if got := Fmt(tt.args.text); got != tt.want {
-			t.Errorf("%s: Format(%q) = %q, want %q", tt.name, tt.args.text, got, tt.want)
+	for _, tt := range testsFormat {
+		if got := Fmt(tt.test); got != tt.want {
+			t.Errorf("%s: Format(%q) = %q, want %q", tt.name, tt.test, got, tt.want)
 		}
 	}
+}
+
+var testsStripFormat = []struct {
+	name string
+	test string
+	want string
+}{
+	{name: "start, end", test: "{red}test{c}", want: "test"},
+	{name: "start, middle, end", test: "{red}te{red}st{c}", want: "test"},
+	{name: "partial", test: "{redtest{c}", want: "{redtest"},
+	{name: "inside", test: "{re{c}d}test{c}", want: "{red}test"},
+	{name: "nothing", test: "this is a test.", want: "this is a test."},
+}
+
+func FuzzStripFormat(f *testing.F) {
+	for _, tc := range testsStripFormat {
+		f.Add(tc.test)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		got := TrimFmt(orig)
+		got2 := TrimFmt(got)
+
+		if utf8.ValidString(orig) {
+			if !utf8.ValidString(got) {
+				t.Errorf("produced invalid UTF-8 string %q", got)
+			}
+
+			if !utf8.ValidString(got2) {
+				t.Errorf("produced invalid UTF-8 string %q", got2)
+			}
+		}
+	})
 }
 
 func TestStripFormat(t *testing.T) {
-	type args struct {
-		text string
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{name: "start, end", args: args{text: "{red}test{c}"}, want: "test"},
-		{name: "start, middle, end", args: args{text: "{red}te{red}st{c}"}, want: "test"},
-		{name: "partial", args: args{text: "{redtest{c}"}, want: "{redtest"},
-		{name: "inside", args: args{text: "{re{c}d}test{c}"}, want: "{red}test"},
-		{name: "nothing", args: args{text: "this is a test."}, want: "this is a test."},
-	}
-
-	for _, tt := range tests {
-		if got := TrimFmt(tt.args.text); got != tt.want {
-			t.Errorf("%s: StripFormat(%q) = %q, want %q", tt.name, tt.args.text, got, tt.want)
+	for _, tt := range testsStripFormat {
+		if got := TrimFmt(tt.test); got != tt.want {
+			t.Errorf("%s: StripFormat(%q) = %q, want %q", tt.name, tt.test, got, tt.want)
 		}
 	}
+}
+
+var testsStripRaw = []struct {
+	name string
+	test string // gets passed to Format() before sent
+	want string
+}{
+	{name: "start, end", test: "{red}{b}test{c}", want: "test"},
+	{name: "start, end in numbers", test: "{red}1234{c}", want: "1234"},
+	{name: "start, middle, end", test: "{red}te{red}st{c}", want: "test"},
+	{name: "partial", test: "{redtest{c}", want: "{redtest"},
+	{name: "inside", test: "{re{c}d}test{c}", want: "{red}test"},
+	{name: "fg+bg colors start", test: "{red,yellow}test{c}", want: "test"},
+	{name: "fg+bg colors start in numbers", test: "{red,yellow}1234{c}", want: "1234"},
+	{name: "fg+bg colors end", test: "test{,yellow}", want: "test"},
+	{name: "bg colors start", test: "{,yellow}test{c}", want: "test"},
+	{name: "inside", test: "{re{c}d}test{c}", want: "{red}test"},
+	{name: "nothing", test: "this is a test.", want: "this is a test."},
+}
+
+func FuzzStripRaw(f *testing.F) {
+	for _, tc := range testsStripRaw {
+		f.Add(tc.test)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		got := StripRaw(orig)
+		got2 := StripRaw(got)
+
+		if utf8.ValidString(orig) {
+			if !utf8.ValidString(got) {
+				t.Errorf("produced invalid UTF-8 string %q", got)
+			}
+
+			if !utf8.ValidString(got2) {
+				t.Errorf("produced invalid UTF-8 string %q", got2)
+			}
+		}
+	})
 }
 
 func TestStripRaw(t *testing.T) {
-	type args struct {
-		text string
-	}
-
-	tests := []struct {
-		name string
-		args args // gets passed to Format() before sent
-		want string
-	}{
-		{name: "start, end", args: args{text: "{red}{b}test{c}"}, want: "test"},
-		{name: "start, end in numbers", args: args{text: "{red}1234{c}"}, want: "1234"},
-		{name: "start, middle, end", args: args{text: "{red}te{red}st{c}"}, want: "test"},
-		{name: "partial", args: args{text: "{redtest{c}"}, want: "{redtest"},
-		{name: "inside", args: args{text: "{re{c}d}test{c}"}, want: "{red}test"},
-		{name: "fg+bg colors start", args: args{text: "{red,yellow}test{c}"}, want: "test"},
-		{name: "fg+bg colors start in numbers", args: args{text: "{red,yellow}1234{c}"}, want: "1234"},
-		{name: "fg+bg colors end", args: args{text: "test{,yellow}"}, want: "test"},
-		{name: "bg colors start", args: args{text: "{,yellow}test{c}"}, want: "test"},
-		{name: "inside", args: args{text: "{re{c}d}test{c}"}, want: "{red}test"},
-		{name: "nothing", args: args{text: "this is a test."}, want: "this is a test."},
-	}
-
-	for _, tt := range tests {
-		if got := StripRaw(Fmt(tt.args.text)); got != tt.want {
-			t.Fatalf("%s: StripRaw(%q) = %q, want %q", tt.name, tt.args.text, got, tt.want)
+	for _, tt := range testsStripRaw {
+		if got := StripRaw(Fmt(tt.test)); got != tt.want {
+			t.Fatalf("%s: StripRaw(%q) = %q, want %q", tt.name, tt.test, got, tt.want)
 		}
 	}
+}
+
+var testsValidNick = []struct {
+	name string
+	test string
+	want bool
+}{
+	{name: "normal", test: "test", want: true},
+	{name: "empty", test: "", want: false},
+	{name: "hyphen and special", test: "test[-]", want: true},
+	{name: "invalid middle", test: "test!test", want: false},
+	{name: "invalid dot middle", test: "test.test", want: false},
+	{name: "end", test: "test!", want: false},
+	{name: "invalid start", test: "!test", want: false},
+	{name: "backslash and numeric", test: "test[\\0", want: true},
+	{name: "long", test: "test123456789AZBKASDLASMDLKM", want: true},
+	{name: "index 0 dash", test: "-test", want: false},
+	{name: "index 0 numeric", test: "0test", want: false},
+	{name: "RFC1459 non-lowercase-converted", test: "test^", want: true},
+	{name: "RFC1459 non-lowercase-converted", test: "test~", want: false},
+}
+
+func FuzzValidNick(f *testing.F) {
+	for _, tc := range testsValidNick {
+		f.Add(tc.test)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		_ = IsValidNick(orig)
+	})
 }
 
 func TestIsValidNick(t *testing.T) {
-	type args struct {
-		nick string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{name: "normal", args: args{nick: "test"}, want: true},
-		{name: "empty", args: args{nick: ""}, want: false},
-		{name: "hyphen and special", args: args{nick: "test[-]"}, want: true},
-		{name: "invalid middle", args: args{nick: "test!test"}, want: false},
-		{name: "invalid dot middle", args: args{nick: "test.test"}, want: false},
-		{name: "end", args: args{nick: "test!"}, want: false},
-		{name: "invalid start", args: args{nick: "!test"}, want: false},
-		{name: "backslash and numeric", args: args{nick: "test[\\0"}, want: true},
-		{name: "long", args: args{nick: "test123456789AZBKASDLASMDLKM"}, want: true},
-		{name: "index 0 dash", args: args{nick: "-test"}, want: false},
-		{name: "index 0 numeric", args: args{nick: "0test"}, want: false},
-		{name: "RFC1459 non-lowercase-converted", args: args{nick: "test^"}, want: true},
-		{name: "RFC1459 non-lowercase-converted", args: args{nick: "test~"}, want: false},
-	}
-	for _, tt := range tests {
-		if got := IsValidNick(tt.args.nick); got != tt.want {
-			t.Errorf("%s: IsValidNick(%q) = %v, want %v", tt.name, tt.args.nick, got, tt.want)
+	for _, tt := range testsValidNick {
+		if got := IsValidNick(tt.test); got != tt.want {
+			t.Errorf("%s: IsValidNick(%q) = %v, want %v", tt.name, tt.test, got, tt.want)
 		}
 	}
+}
+
+var testsValidChannel = []struct {
+	name string
+	test string
+	want bool
+}{
+	{name: "valid channel", test: "#valid", want: true},
+	{name: "invalid channel comma", test: "#invalid,", want: false},
+	{name: "invalid channel space", test: "#inva lid", want: false},
+	{name: "valid channel with numerics", test: "#1valid0", want: true},
+	{name: "valid channel with special", test: "#valid[]test", want: true},
+	{name: "valid channel with special", test: "#[]valid[]test[]", want: true},
+	{name: "just hash", test: "#", want: false},
+	{name: "empty", test: "", want: false},
+	{name: "invalid prefix", test: "$invalid", want: false},
+	{name: "too long", test: "#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", want: false},
+	{name: "valid id prefix", test: "!12345test", want: true},
+	{name: "invalid id length", test: "!1234", want: false},
+	{name: "invalid id length", test: "!12345", want: false},
+	{name: "invalid id prefix", test: "!test1invalid", want: false},
+}
+
+func FuzzValidChannel(f *testing.F) {
+	for _, tc := range testsValidChannel {
+		f.Add(tc.test)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		_ = IsValidChannel(orig)
+	})
 }
 
 func TestIsValidChannel(t *testing.T) {
-	type args struct {
-		channel string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{name: "valid channel", args: args{channel: "#valid"}, want: true},
-		{name: "invalid channel comma", args: args{channel: "#invalid,"}, want: false},
-		{name: "invalid channel space", args: args{channel: "#inva lid"}, want: false},
-		{name: "valid channel with numerics", args: args{channel: "#1valid0"}, want: true},
-		{name: "valid channel with special", args: args{channel: "#valid[]test"}, want: true},
-		{name: "valid channel with special", args: args{channel: "#[]valid[]test[]"}, want: true},
-		{name: "just hash", args: args{channel: "#"}, want: false},
-		{name: "empty", args: args{channel: ""}, want: false},
-		{name: "invalid prefix", args: args{channel: "$invalid"}, want: false},
-		{name: "too long", args: args{channel: "#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}, want: false},
-		{name: "valid id prefix", args: args{channel: "!12345test"}, want: true},
-		{name: "invalid id length", args: args{channel: "!1234"}, want: false},
-		{name: "invalid id length", args: args{channel: "!12345"}, want: false},
-		{name: "invalid id prefix", args: args{channel: "!test1invalid"}, want: false},
-	}
-	for _, tt := range tests {
-		if got := IsValidChannel(tt.args.channel); got != tt.want {
-			t.Errorf("%s: IsValidChannel(%q) = %v, want %v", tt.name, tt.args.channel, got, tt.want)
+	for _, tt := range testsValidChannel {
+		if got := IsValidChannel(tt.test); got != tt.want {
+			t.Errorf("%s: IsValidChannel(%q) = %v, want %v", tt.name, tt.test, got, tt.want)
 		}
 	}
+}
+
+var testsValidUser = []struct {
+	name string
+	test string
+	want bool
+}{
+	{name: "user without ident server", test: "~test", want: true},
+	{name: "user with ident server", test: "test", want: true},
+	{name: "non-alphanumeric first index", test: "-test", want: false},
+	{name: "non-alphanumeric first index", test: "[test]", want: false},
+	{name: "numeric first index", test: "0test", want: true},
+	{name: "blank", test: "", want: false},
+	{name: "just tilde", test: "~", want: false},
+	{name: "special chars", test: "test-----", want: true},
+	{name: "special chars", test: "test-[]-", want: true},
+	{name: "special chars, invalid after first index", test: "t!--", want: false},
+}
+
+func FuzzValidUser(f *testing.F) {
+	for _, tc := range testsValidUser {
+		f.Add(tc.test)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		_ = IsValidUser(orig)
+	})
 }
 
 func TestIsValidUser(t *testing.T) {
-	type args struct {
-		name string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{name: "user without ident server", args: args{name: "~test"}, want: true},
-		{name: "user with ident server", args: args{name: "test"}, want: true},
-		{name: "non-alphanumeric first index", args: args{name: "-test"}, want: false},
-		{name: "non-alphanumeric first index", args: args{name: "[test]"}, want: false},
-		{name: "numeric first index", args: args{name: "0test"}, want: true},
-		{name: "blank", args: args{name: ""}, want: false},
-		{name: "just tilde", args: args{name: "~"}, want: false},
-		{name: "special chars", args: args{name: "test-----"}, want: true},
-		{name: "special chars", args: args{name: "test-[]-"}, want: true},
-		{name: "special chars, invalid after first index", args: args{name: "t!--"}, want: false},
-	}
-	for _, tt := range tests {
-		if got := IsValidUser(tt.args.name); got != tt.want {
-			t.Errorf("%s: IsValidUser(%q) = %v, want %v", tt.name, tt.args.name, got, tt.want)
+	for _, tt := range testsValidUser {
+		if got := IsValidUser(tt.test); got != tt.want {
+			t.Errorf("%s: IsValidUser(%q) = %v, want %v", tt.name, tt.test, got, tt.want)
 		}
 	}
 }
 
-func TestToRFC1459(t *testing.T) {
-	cases := []struct {
-		in   string
-		want string
-	}{
-		{"", ""},
-		{"a", "a"},
-		{"abcd", "abcd"},
-		{"AbcD", "abcd"},
-		{"!@#$%^&*()_+-=", "!@#$%~&*()_+-="},
-		{"Abcd[]", "abcd{}"},
+var testsToRFC1459 = []struct {
+	in   string
+	want string
+}{
+	{"", ""},
+	{"a", "a"},
+	{"abcd", "abcd"},
+	{"AbcD", "abcd"},
+	{"!@#$%^&*()_+-=", "!@#$%~&*()_+-="},
+	{"Abcd[]", "abcd{}"},
+}
+
+func FuzzToRFC1459(f *testing.F) {
+	for _, tc := range testsToRFC1459 {
+		f.Add(tc.in)
 	}
 
-	for _, tt := range cases {
+	f.Fuzz(func(t *testing.T, orig string) {
+		got := ToRFC1459(orig)
+
+		if utf8.ValidString(orig) && !utf8.ValidString(got) {
+			t.Errorf("produced invalid UTF-8 string %q", got)
+		}
+	})
+}
+
+func TestToRFC1459(t *testing.T) {
+	for _, tt := range testsToRFC1459 {
 		if got := ToRFC1459(tt.in); got != tt.want {
 			t.Errorf("ToRFC1459() = %q, want %q", got, tt.want)
 		}
@@ -308,27 +398,37 @@ func TestPatternWithoutGlobs(t *testing.T) {
 	testGlobMatch(t, "test", "test")
 }
 
-func TestGlob(t *testing.T) {
-	cases := []string{
-		"*test",           // Leading.
-		"this*",           // Trailing.
-		"this*test",       // Middle.
-		"*is *",           // String in between two.
-		"*is*a*",          // Lots.
-		"**test**",        // Double glob characters.
-		"**is**a***test*", // Varying number.
-		"* *",             // White space between.
-		"*",               // Lone.
-		"**********",      // Nothing but globs.
-		"*Ѿ*",             // Unicode.
-		"*is a ϗѾ *",      // Mixed ASCII/unicode.
+var testsGlob = []string{
+	"*test",           // Leading.
+	"this*",           // Trailing.
+	"this*test",       // Middle.
+	"*is *",           // String in between two.
+	"*is*a*",          // Lots.
+	"**test**",        // Double glob characters.
+	"**is**a***test*", // Varying number.
+	"* *",             // White space between.
+	"*",               // Lone.
+	"**********",      // Nothing but globs.
+	"*Ѿ*",             // Unicode.
+	"*is a ϗѾ *",      // Mixed ASCII/unicode.
+}
+
+func FuzzGlob(f *testing.F) {
+	for _, tc := range testsGlob {
+		f.Add(tc, tc)
 	}
 
-	for _, pattern := range cases {
+	f.Fuzz(func(t *testing.T, orig, orig2 string) {
+		_ = Glob(orig, orig2)
+	})
+}
+
+func TestGlob(t *testing.T) {
+	for _, pattern := range testsGlob {
 		testGlobMatch(t, "this is a ϗѾ test", pattern)
 	}
 
-	cases = []string{
+	cases := []string{
 		"test*", // Implicit substring match.
 		"*is",   // Partial match.
 		"*no*",  // Globs without a match between them.
